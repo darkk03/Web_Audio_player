705e018a287548b67ecdcef418f8806345c624d9
darkk03 <towboatqq@gmail.com>
2023-09-13 17:17:31 +0200

    volumen_and_playpause

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@
 const seekbar = document.querySelector('.seekbar');
 const volumeIcon = document.getElementById('volumeIcon');
 let isMuted = false; // Объявляем флаг "mute" и устанавливаем его в false (звук не включен)
+let previousVolume = 50; // Сохраняем предыдущее значение уровня громкости
 
 // Функция для обновления иконки уровня громкости
 function updateVolumeIcon() {
   const seekbarValue = seekbar.value;
-  if (isMuted || seekbarValue === 0) {
+  if (isMuted || seekbarValue === '0') {
     volumeIcon.src = 'images/Mute.png'; // Если включен режим "mute" или уровень громкости на нуле
   } else if (seekbarValue <= 50) {
     volumeIcon.src = 'images/Volume Down.png'; // При уровне меньше или равном 50%
@@ -25,7 +26,10 @@ function handleSeekBar() {
 volumeIcon.addEventListener('click', () => {
   isMuted = !isMuted; // Инвертируем флаг "mute"
   if (isMuted) {
-    seekbar.value = 0; // Устанавливаем уровень громкости на ноль при включении "mute"
+    previousVolume = seekbar.value; // Сохраняем текущее значение уровня громкости
+    seekbar.value = '0'; // Устанавливаем уровень громкости на ноль при включении "mute"
+  } else {
+    seekbar.value = previousVolume; // Возвращаем предыдущее значение уровня громкости
   }
   handleSeekBar(); // Обновляем иконку после изменения состояния звука
 });
@@ -35,3 +39,21 @@ seekbar.addEventListener('input', handleSeekBar);
 
 // Инициализируем иконку уровня громкости при загрузке страницы
 updateVolumeIcon();
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const playPauseButton = document.querySelector(".btn._play img");
+  let isPlaying = false;
+
+  playPauseButton.addEventListener("click", function () {
+    if (isPlaying) {
+      playPauseButton.src = "images/Play.png";
+      isPlaying = false;
+    } else {
+      playPauseButton.src = "images/Pause.png";
+      isPlaying = true;
+    }
+  });
+});
